071c9407a00b1245052529224484ff0481dfd645
Max <github-mg@guenther.cloud>
2022-01-03 19:58:19 +0100

    Add init() method
    This method prepares the program for tracking the work

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-def main():
-    pass
+import os
+
+
+def init():
+    if os.path.isfile("tracker.csv"):
+        return
+    else:
+        # TODO: Create file
+        pass
 
 
 if __name__ == "__main__":
-    main()
+    init()
