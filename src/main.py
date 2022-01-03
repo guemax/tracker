92653c4f68bd4bf46d07382ad68c6d9ada3a54d2
Max <github-mg@guenther.cloud>
2022-01-03 20:03:13 +0100

    Create tracker file if not exists

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 import os
 
 
+def create_tracker_file(filename: str) -> None:
+    with open(filename, "w") as f:
+        pass
+
+
+def file_exists(filename: str) -> bool:
+    return os.path.isfile(filename)
+
+
 def init():
-    if os.path.isfile("tracker.csv"):
+    filename = "tracker.csv"
+    if file_exists(filename):
         return
     else:
-        # TODO: Create file
-        pass
+        create_tracker_file(filename)
 
 
 if __name__ == "__main__":
