ce37a53221b309a2b81abb1f7c190fa4fffef9ef
Max <github-mg@guenther.cloud>
2022-01-04 12:02:04 +0100

    Refactor tests for CSV handler

--- tests/test_csv_handler/test_csv_handler.py
+++ tests/test_csv_handler/test_csv_handler.py
@@ -15,10 +15,7 @@ class TestCSVHandler(unittest.TestCase):
     def test_inititalizing_tracker_file(self):
         self.clean_and_init_tracker_file()
 
-        with open(self.csv_handler.tracker_file, "r") as f:
-            file = f.read()
-            file = file.strip()
-
+        file = self.get_contents_of_tracker_file()
         self.assertEqual(file, "start_time,stop_time,message")
 
     def test_creating_a_new_entry(self):
@@ -26,11 +23,15 @@ class TestCSVHandler(unittest.TestCase):
 
         self.csv_handler.create_new_entry()
 
-        with open(self.csv_handler.tracker_file, "r") as f:
-            file = f.read()
-            file = file.strip()
-
+        file = self.get_contents_of_tracker_file()
         self.assertTrue(self.pattern_new_entry.match(file))
+
+    def test_checking_if_tracker_file_exists(self):
+        self.remove_tracker_file()
+        self.assertFalse(self.csv_handler.tracker_file_exists())
+
+        self.csv_handler.create_tracker_file()
+        self.assertTrue(self.csv_handler.tracker_file_exists())
 
     def remove_tracker_file(self):
         try:
@@ -42,3 +43,14 @@ class TestCSVHandler(unittest.TestCase):
     def clean_and_init_tracker_file(self):
         self.remove_tracker_file()
         self.csv_handler.init_tracker_csv_file()
+
+    def get_contents_of_tracker_file(self) -> str:
+        with open(self.csv_handler.tracker_file, "r") as f:
+            file = f.read()
+            file = file.strip()
+
+        return file
+
+
+if __name__ == "__main__":
+    unittest.main()
