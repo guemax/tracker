5755c4ff2d83acfe874c631c1b3b725f5c1b9d0a
Max <github-mg@guenther.cloud>
2022-01-05 19:05:03 +0100

    Implement exception hadnling in start() function

--- src/start/start.py
+++ src/start/start.py
@@ -3,13 +3,19 @@ import logging
 import click
 
 from ..csv_handler import CSVHandler
+from ..exceptions.UnfinishedEntryPresentException import UnfinishedEntryPresentException
 
 
 @click.command()
 def start():
     """Start a new timer"""
     csv_handler = CSVHandler.CSVHandler()
-    time = csv_handler.create_new_entry()
-
-    logging.info(f"New timer started")
-    print(f"New timer started at {time}\nOK")
+    try:
+        time = csv_handler.create_new_entry()
+    except UnfinishedEntryPresentException:
+        logging.info("Starting a new timer aborted due to an exisiting timer")
+        print("A timer already exists which has not been stopped yet.\n"
+              "Please stop it by typing 'tracker stop'.")
+    else:
+        logging.info(f"New timer started")
+        print(f"New timer started at {time}\nOK")
