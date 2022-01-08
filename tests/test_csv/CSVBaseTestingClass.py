6c05e6606c7d692f165065ed562ece0170101461
Max <github-mg@guenther.cloud>
2022-01-08 17:02:54 +0100

    Refactor order of functions in CSV base testing class

--- tests/test_csv/CSVBaseTestingClass.py
+++ tests/test_csv/CSVBaseTestingClass.py
@@ -10,12 +10,16 @@ class CSVBaseTestingClass(unittest.TestCase):
     def setUp(self) -> None:
         self.csv_handler = CSVHandler()
 
+    def check_for_correct_column_names(self) -> None:
+        self.assertTrue(self.columns_names_are_correct())
+
     def columns_names_are_correct(self) -> bool:
         column_names = self.get_column_names()
         return column_names == self.csv_handler.get_column_names()
 
-    def check_for_correct_column_names(self) -> None:
-        self.assertTrue(self.columns_names_are_correct())
+    def get_column_names(self) -> list:
+        data_frame = pandas.read_csv(self.csv_handler.tracker_file)
+        return list(data_frame.columns)
 
     def clean_and_init_tracker_file(self) -> None:
         self.remove_tracker_file()
@@ -28,16 +32,12 @@ class CSVBaseTestingClass(unittest.TestCase):
             # File is not existing, that's good, nothing to do for us.
             pass
 
-    def get_column_names(self) -> list:
-        data_frame = pandas.read_csv(self.csv_handler.tracker_file)
-        return list(data_frame.columns)
-
-    def get_contents_of_tracker_file(self) -> pandas.DataFrame:
-        data_frame = pandas.read_csv(self.csv_handler.tracker_file)
-        return data_frame
-
     def get_contents_of_tracker_file_with_replaced_nans(self) -> pandas.DataFrame:
         data = self.get_contents_of_tracker_file()
         data = data.fillna("")
 
         return data
+
+    def get_contents_of_tracker_file(self) -> pandas.DataFrame:
+        data_frame = pandas.read_csv(self.csv_handler.tracker_file)
+        return data_frame
