d758013a1bc00d2874bb50500686001caf3cc6fd
Max <github-mg@guenther.cloud>
2022-01-08 11:39:44 +0100

    Add info message when no entry was found to be logged

--- src/log/log.py
+++ src/log/log.py
@@ -7,6 +7,13 @@ from ..entry_handler import EntryHandler
 def log():
     """Show old entries grouped by date"""
     entry_handler = EntryHandler.EntryHandler()
-    entries = entry_handler.get_entries()
+    entries_grouped_by_date = entry_handler.get_entries()
+    number_of_grouped_entries = len(entries_grouped_by_date)
 
-    print(entries)
+    if number_of_grouped_entries == 0:
+        print("Nothing to see yet.\n"
+              "Start creating entries by typing 'tracker start' and finish them by typing 'tracker stop'.")
+    else:
+        print(f"Showing all entries grouped by date. ({number_of_grouped_entries} in total).\n"
+              f"Use 'tracker log <ID>' to show all entries of the date with the ID <ID>.\n")
+        print(f"{entries_grouped_by_date}\n")
