552e92d34252522b200cd7b9f4b1bd88b33cfa30
Max <github-mg@guenther.cloud>
2022-01-08 16:38:55 +0100

    Refactor tests for starting a new timer

---
+++ tests/test_click_commands/CommandBaseTestingClass.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+
+from click.testing import CliRunner
+
+from src.main import cli, setup_cli
+from src.csv.CSVHandler import CSVHandler
+
+
+class CommandBaseTestingClass(unittest.TestCase):
+    def setUp(self) -> None:
+        self.runner = CliRunner()
+        self.result = None
+
+        self.csv_handler = CSVHandler()
+
+        setup_cli()
+
+    def run_cli(self, options: list) -> None:
+        self.result = self.runner.invoke(cli, options)
+
+    def clean_and_init_tracker_file(self) -> None:
+        self.remove_tracker_file()
+        self.csv_handler.init_tracker_csv_file()
+
+    def remove_tracker_file(self) -> None:
+        try:
+            os.remove(self.csv_handler.tracker_file)
+        except FileNotFoundError:
+            # File is not existing, that's good, nothing to do for us.
+            pass
--- tests/test_click_commands/test_start/test_start.py
+++ tests/test_click_commands/test_start/test_start.py
@@ -1,26 +1,25 @@
 import unittest
 
-from click.testing import CliRunner
-
-from src.main import cli, setup_cli
+from ..CommandBaseTestingClass import CommandBaseTestingClass
 
 
-class TestStart(unittest.TestCase):
+class TestStart(CommandBaseTestingClass):
     def setUp(self) -> None:
-        self.runner = CliRunner()
-        self.result = None
-
-        setup_cli()
+        super(TestStart, self).setUp()
 
     def test_starting_a_timer(self) -> None:
-        self.result = self.runner.invoke(cli, ["start"])
+        self.clean_and_init_tracker_file()
+
+        self.run_cli(["start"])
         self.assertEqual(self.result.exit_code, 0)
 
         self.assertIn("New timer started", self.result.output)
         self.assertIn("OK", self.result.output)
 
     def test_starting_a_timer_with_invalid_option(self) -> None:
-        self.result = self.runner.invoke(cli, ["start", "--unknown-option"])
+        self.clean_and_init_tracker_file()
+
+        self.run_cli(["start", "--unknown-option"])
         self.assertEqual(self.result.exit_code, 2)
 
         self.assertIn("Error", self.result.output)
