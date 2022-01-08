8a705f582f2108894f491a6766c652f4130d67f7
Max <github-mg@guenther.cloud>
2022-01-08 11:40:06 +0100

    Add tests for entry handler

---
+++ tests/test_entry_handler/test_entry_handler.py
@@ -0,0 +1,45 @@
+import unittest
+
+from src.entry_handler.EntryHandler import EntryHandler
+from src.timer_handler.TimerHandler import TimerHandler
+
+from tests.test_csv.CSVBaseTestingClass import CSVBaseTestingClass
+
+
+class TestEntryHandler(CSVBaseTestingClass):
+    def setUp(self) -> None:
+        super(TestEntryHandler, self).setUp()
+        self.entry_handler = EntryHandler()
+        self.timer_handler = TimerHandler()
+
+    def test_logging_without_any_entries(self) -> None:
+        self.clean_and_init_tracker_file()
+        self.assertEqual(self.get_length_of_entries(), 0)
+
+    def test_logging_with_more_entries(self) -> None:
+        self.clean_and_init_tracker_file()
+
+        self.add_new_entry()
+        self.assertEqual(self.get_length_of_entries(), 1)
+
+        self.timer_handler.start_timer()
+        self.timer_handler.stop_timer("")
+
+        # Entries are grouped by date, so adding two or more entries at the same day won't increase the length
+        self.add_new_entry()
+        self.assertEqual(self.get_length_of_entries(), 1)
+
+        self.add_new_entry()
+        self.add_new_entry()
+        self.assertEqual(self.get_length_of_entries(), 1)
+
+    def get_length_of_entries(self) -> int:
+        return len(self.entry_handler.get_entries())
+
+    def add_new_entry(self):
+        self.timer_handler.start_timer()
+        self.timer_handler.stop_timer("")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
