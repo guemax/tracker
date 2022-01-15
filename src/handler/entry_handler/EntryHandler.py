a17bc90cae61d4d2b8c8c61116f3ee898e3253b2
Max <github-mg@guenther.cloud>
2022-01-15 17:38:42 +0100

    Refactor the EntryHandler

--- src/handler/entry_handler/EntryHandler.py
+++ src/handler/entry_handler/EntryHandler.py
@@ -24,42 +24,17 @@ class EntryHandler(BaseEntryHandlerClass):
         self.entries = self.data.get_group(date)
         self.change_index()
 
-        self.entries = self.rename_columns()
         self.merge_date_and_time_columns()
 
         self.remove_start_and_stop_date_and_start_and_stop_time_columns()
         self.reorder_columns()
 
+        self.entries = self.rename_columns()
+
         # Make pandas print a message over more than one line in the console
         pandas.set_option("display.max_colwidth", None)
 
         return [date, self.entries]
-
-    def rename_columns(self) -> pandas.DataFrame:
-        entries_with_renamed_columns = self.entries \
-            .rename(columns={
-                                "start_date": "Start Date", "start_time": "Start Time",
-                                "stop_date": "Stop Date", "stop_time": "Stop Time",
-                                "work_hours": "Work hours", "message": "Message"
-                            }
-                    )
-        return entries_with_renamed_columns
-
-    def remove_start_and_stop_date_and_start_and_stop_time_columns(self) -> None:
-        self.entries = self.entries.drop(columns=["Start Date", "Start Time", "Stop Date", "Stop Time"])
-
-    def reorder_columns(self) -> None:
-        self.entries = self.entries[["Start", "Stop", "Work hours", "Message"]]
-
-    def merge_date_and_time_columns(self) -> None:
-        self.merge_start_date_and_start_time()
-        self.merge_stop_date_and_stop_time()
-
-    def merge_start_date_and_start_time(self) -> None:
-        self.entries["Start"] = self.entries[["Start Date", "Start Time"]].apply(lambda x: " at ".join(x), axis=1)
-
-    def merge_stop_date_and_stop_time(self) -> None:
-        self.entries["Stop"] = self.entries[["Stop Date", "Stop Time"]].apply(lambda x: " at ".join(x), axis=1)
 
     def get_string_of_date_by_id(self, id_of_date: int) -> str:
         entries_grouped_by_date = self.grouped_entry_handler.get_entries_grouped_by_date()
@@ -76,3 +51,34 @@ class EntryHandler(BaseEntryHandlerClass):
 
     def boost_index(self) -> None:
         self.entries.index += 1
+
+    def rename_columns(self) -> pandas.DataFrame:
+        entries_with_renamed_columns = self.entries \
+            .rename(columns={
+                                "start": "Start",
+                                "stop": "Stop",
+                                "work_hours": "Work hours", "message": "Message"
+                            }
+                    )
+        return entries_with_renamed_columns
+
+    def merge_date_and_time_columns(self) -> None:
+        # Below is just work around for pandas' error: pandas.core.common.SettingWithCopyError
+        pandas.set_option("mode.chained_assignment", None)
+
+        self.merge_start_date_and_start_time()
+        self.merge_stop_date_and_stop_time()
+
+        pandas.set_option("mode.chained_assignment", "warn")
+
+    def merge_start_date_and_start_time(self) -> None:
+        self.entries.loc[:, "start"] = self.entries["start_date"] + " at " + self.entries["start_time"]
+
+    def merge_stop_date_and_stop_time(self) -> None:
+        self.entries.loc[:, "stop"] = self.entries["stop_date"] + " at " + self.entries["stop_time"]
+
+    def remove_start_and_stop_date_and_start_and_stop_time_columns(self) -> None:
+        self.entries = self.entries.drop(columns=["start_date", "start_time", "stop_date", "stop_time"])
+
+    def reorder_columns(self) -> None:
+        self.entries = self.entries[["start", "stop", "work_hours", "message"]]
