a927bc649374d4276dffd41eb611c0c2aa1d8022
Max <github-mg@guenther.cloud>
2022-01-15 12:35:19 +0100

    Refactor tests for entry handler

--- tests/test_handler/test_entry_handler/test_entry_handler.py
+++ tests/test_handler/test_entry_handler/test_entry_handler.py
@@ -23,42 +23,61 @@ class TestEntryHandler(CSVBaseTestingClass):
         setup_test_values()
 
         entries = self.entry_handler.get_entries_of_specific_date(1)
+        date = entries[0]
+        entries_of_date = entries[1]
 
-        self.assertEqual(type(entries[0]), str)
-        self.assertEqual(type(entries[1]), pandas.DataFrame)
+        self.assertEqual(type(date), str)
+        self.assertEqual(type(entries_of_date), pandas.DataFrame)
 
-        self.assertIn(entries[0], entries[1].at[1, "Start"])
+        self.assertIn(date, entries_of_date.at[1, "Start"])
+
+    def test_logging_entries_of_specific_date_without_any_entries(self) -> None:
+        self.clean_and_init_tracker_file()
+
+        self.check_for_invalid_id_of_date_exception(0)
+        self.check_for_invalid_id_of_date_exception(1)
+        self.check_for_invalid_id_of_date_exception(2)
+
+    def check_for_invalid_id_of_date_exception(self, id_of_date: int) -> None:
+        self.assertRaises(InvalidIDOfDateException, self.entry_handler.get_entries_of_specific_date, id_of_date)
 
     def test_logging_entries_of_soecific_date_with_unknown_id(self) -> None:
         self.clean_and_init_tracker_file()
         setup_test_values()
 
-        self.assertRaises(InvalidIDOfDateException, self.entry_handler.get_entries_of_specific_date, 0)
-        self.assertRaises(InvalidIDOfDateException, self.entry_handler.get_entries_of_specific_date, 100)
-        self.assertRaises(InvalidIDOfDateException, self.entry_handler.get_entries_of_specific_date, -1)
+        self.check_for_invalid_id_of_date_exception(0)
+        self.check_for_invalid_id_of_date_exception(-1)
+        self.check_for_invalid_id_of_date_exception(100)
 
-    def test_logging_without_any_entries(self) -> None:
+    def test_logging_without_any_grouped_entries(self) -> None:
         self.clean_and_init_tracker_file()
-        self.assertEqual(self.get_length_of_entries(), 0)
+        self.assertEqual(self.get_length_of_grouped_entries(), 0)
 
-    def test_logging_with_more_entries(self) -> None:
+    def get_length_of_grouped_entries(self) -> int:
+        return len(self.entry_handler.get_entries_grouped_by_date())
+
+    def test_logging_with_more_grouped_entries(self) -> None:
         self.clean_and_init_tracker_file()
 
         self.add_new_entry()
-        self.assertEqual(self.get_length_of_entries(), 1)
+        self.assertEqual(self.get_length_of_grouped_entries(), 1)
 
         self.timer_handler.start_timer()
         self.timer_handler.stop_timer("")
 
         # Entries are grouped by date, so adding two or more entries at the same day won't increase the length
         self.add_new_entry()
-        self.assertEqual(self.get_length_of_entries(), 1)
+        self.assertEqual(self.get_length_of_grouped_entries(), 1)
 
         self.add_new_entry()
         self.add_new_entry()
-        self.assertEqual(self.get_length_of_entries(), 1)
+        self.assertEqual(self.get_length_of_grouped_entries(), 1)
 
-    def test_column_names(self):
+    def add_new_entry(self):
+        self.timer_handler.start_timer()
+        self.timer_handler.stop_timer("")
+
+    def test_column_names_of_grouped_entries(self):
         self.clean_and_init_tracker_file()
         self.add_new_entry()
 
@@ -67,13 +86,6 @@ class TestEntryHandler(CSVBaseTestingClass):
 
         index_name = self.entry_handler.get_entries_grouped_by_date().index.name
         self.assertEqual(index_name, "ID")
-
-    def get_length_of_entries(self) -> int:
-        return len(self.entry_handler.get_entries_grouped_by_date())
-
-    def add_new_entry(self):
-        self.timer_handler.start_timer()
-        self.timer_handler.stop_timer("")
 
 
 if __name__ == "__main__":  # pragma: no cover
