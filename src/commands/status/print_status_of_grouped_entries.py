23993fba3d11c1cbc2622319c5c4d6f74e307146
Max <github-mg@guenther.cloud>
2022-01-19 17:28:37 +0100

    Use the GroupedEntryHandler for displaying status about the grouped entries

--- src/commands/status/print_status_of_grouped_entries.py
+++ src/commands/status/print_status_of_grouped_entries.py
@@ -1,13 +1,13 @@
 from num2words import num2words
 
-from src.handler.entry_handler.EntryHandler import EntryHandler
+from src.handler.entry_handler.GroupedEntryHandler import GroupedEntryHandler
 from .print_list_item import print_list_item
 
 
 def print_status_of_grouped_entries():
-    entry_handler = EntryHandler()
+    grouped_entry_handler = GroupedEntryHandler()
 
-    number_of_grouped_entries = len(entry_handler.get_entries_grouped_by_date())
+    number_of_grouped_entries = len(grouped_entry_handler.get_entries_grouped_by_date())
     number_of_grouped_entries_as_word = num2words(number_of_grouped_entries).title()
 
     if number_of_grouped_entries == 0:
