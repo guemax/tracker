ed0709496f27e47e30c32e58d6484e948606e84b
Max <github-mg@guenther.cloud>
2022-01-21 17:25:45 +0100

    Move the SetupTestValues class into its own file

--- src/setup_test_values/setup_test_values.py
+++ src/setup_test_values/setup_test_values.py
@@ -1,11 +1,7 @@
 from datetime import datetime
 import random
 
-import click
-import num2words
-
-from .csv import CSVHandler
-from src.console_logger.console_logger import info, warn
+from src.csv import CSVHandler
 
 
 class SetupTestValues:
@@ -32,78 +28,57 @@ class SetupTestValues:
     def setup(self) -> None:
         with open(self.csv_handler.tracker_file, "w") as f:
             f.write(self.header())
-            f.write(self.test_value_lines())
+            f.write(self.entries())
 
     def header(self) -> str:
         return ",".join(self.csv_handler.column_names) + "\n"
 
-    def test_value_lines(self) -> str:
-        lines = ""
+    def entries(self) -> str:
+        entries = ""
 
         for i in range(1, self.number_of_entries + 1):
-            lines += self.build_test_value_line(i)
+            entries += self.build_entry(i)
 
-        return lines
+        return entries
 
-    def build_test_value_line(self, number: int) -> str:
+    def build_entry(self, number: int) -> str:
+        # CREATE HOURS
         start_hour = number
         stop_hour = number + random.choice([0, 1])
 
         # Just leave it as it is
+        max_start_minutes = 58
+        # Just leave it as it is
         max_start_seconds = 58
 
-        start_minutes = random.randint(0, max_start_seconds)
+        # CREATE MINUTES
+        start_minutes = random.randint(0, max_start_minutes)
         stop_minutes = start_minutes + random.randint(1, 59 - start_minutes)
 
+        # CREATE SECONDS
         start_seconds = random.randint(0, max_start_seconds)
         stop_seconds = start_seconds + random.randint(1, 59 - start_seconds)
 
+        # CREATE DAY NUMBER
         if number % 2 == 0:
             date = int(number / 2)
         else:
             date = int((number + 1) / 2)
 
+        # BUILD FULL DATE/TIME
         start_date = f"\"{self.month}, {date} {self.year}\""
         start_time = f"{start_hour:02d}:{start_minutes:02d}:{start_seconds:02d}"
 
         stop_date = start_date
         stop_time = f"{stop_hour:02d}:{stop_minutes:02d}:{stop_seconds:02d}"
 
+        # BUILD WORK HOURS
         date_format = "%H:%M:%S"
         work_hours = datetime.strptime(stop_time, date_format) - datetime.strptime(start_time, date_format)
 
+        # BUILD MESSAGE
         message = random.choice(self.messages)
 
+        # BUILD FINAL ENTRY
         line = f"{start_date},{start_time},{stop_date},{stop_time},{work_hours},{message}\n"
         return line
-
-
-def handler_invalid_number_of_entries() -> None:
-    warn("The number of entries cannot be negative. Please specify a value greater than or equal zero.\n"
-         "EXIT")
-    exit(-1)
-
-
-@click.command()
-@click.option("-e", "--entries", help="Number of entries to create", type=int, default=4)
-def setup_test_values(entries: int) -> None:
-    set_upper = SetupTestValues()
-
-    try:
-        set_upper.set_number_of_entries(entries)
-    except ValueError:
-        handler_invalid_number_of_entries()
-
-    set_upper.setup()
-
-    entries_as_word = num2words.num2words(entries)
-    message = "\nSuccesfully setup"
-    if entries == 1:
-        info(f"{message} one entry.")
-    else:
-        info(f"{message} {entries_as_word} entries.")
-    info("OK")
-
-
-if __name__ == "__main__":  # pragma: no cover
-    setup_test_values()
