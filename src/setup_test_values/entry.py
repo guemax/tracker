fd7741c1b11a8b104e2b10f816fe56d782e4efcb
Max <github-mg@guenther.cloud>
2022-01-21 18:53:40 +0100

    Refactor the Entry classes functions

--- src/setup_test_values/entry.py
+++ src/setup_test_values/entry.py
@@ -1,76 +1,129 @@
 import random
-from datetime import datetime
+from datetime import datetime, time
 
 
 class Entry:
     def __init__(self) -> None:
         self.start_date = ""
-        self.start_time = ""
+        self.start_time = None
 
         self.stop_date = ""
-        self.stop_time = ""
+        self.stop_time = None
 
         self.work_hours = ""
-
         self.message = ""
 
+        self.messages = []
         self.number = 0
 
         self.month = "Jan"
         self.year = "2022"
 
-    def create_datetime(self) -> None:
-        # CREATE HOURS
-        start_hour = random.randint(0, 11)
-        stop_hour = random.randint(start_hour, 12)
-
-        if self.number % 2 == 0:
-            # Second entry of day
-            start_hour += 11
-            stop_hour += 11
-
-        # Just leave them as they are
-        max_minutes = 58
-        max_seconds = 58
-
-        # CREATE MINUTES
-        start_minutes = random.randint(0, max_minutes)
-        stop_minutes = start_minutes + random.randint(1, 59 - start_minutes)
-
-        # CREATE SECONDS
-        start_seconds = random.randint(0, max_seconds)
-        stop_seconds = start_seconds + random.randint(1, 59 - start_seconds)
-
-        # CREATE DAY NUMBER
-        if self.number % 2 == 0:
-            date = int(self.number / 2)
-        else:
-            date = int((self.number + 1) / 2)
-
-        # BUILD FULL DATE/TIME
-        self.start_date = f"\"{self.month}, {date} {self.year}\""
-        self.start_time = f"{start_hour:02d}:{start_minutes:02d}:{start_seconds:02d}"
-
-        self.stop_date = self.start_date
-        self.stop_time = f"{stop_hour:02d}:{stop_minutes:02d}:{stop_seconds:02d}"
+        self.hours = {}
+        self.minutes = {}
+        self.seconds = {}
 
     def build(self, messages: list, number: int) -> str:
         self.number = number
+        self.messages = messages
 
         self.create_datetime()
         self.create_work_hours()
-        # BUILD MESSAGE
-        self.message = random.choice(messages)
+        self.create_message()
 
-        # BUILD FINAL ENTRY
-        line = f"{self.start_date},{self.start_time},{self.stop_date},{self.stop_time},{self.work_hours},{self.message}\n"
+        line = f"{self.start_date},{self.start_time}," \
+               f"{self.stop_date},{self.stop_time}," \
+               f"{self.work_hours},{self.message}" \
+               f"\n"
+
         return line
 
-    def create_work_hours(self) -> None:
-        time_format = "%H:%M:%S"
+    def create_datetime(self) -> None:
+        self.create_date()
+        self.create_time()
 
-        start_time = datetime.strptime(self.start_time, time_format)
-        stop_time = datetime.strptime(self.stop_time, time_format)
+    def create_date(self) -> None:
+        number = 0
+
+        # Count the date in double steps, makes two entries per day
+        if self.is_first_entry_of_day():
+            # 1 -> 1 + 1 / 2 = 1
+            # 3 -> 3 + 1 / 2 = 2
+            # 5 -> 5 + 1 / 2 = 3
+            number = (self.number + 1) / 2
+        elif self.is_second_entry_of_day():
+            # 2 -> 1
+            # 4 -> 2
+            # 6 -> 3
+            number = self.number / 2
+
+        date = int(number / 2)
+
+        self.start_date = f"\"{self.month}, {date} {self.year}\""
+        self.stop_date = self.start_date
+
+    def is_first_entry_of_day(self) -> bool:
+        return not self.is_second_entry_of_day()
+
+    def is_second_entry_of_day(self) -> bool:
+        return self.number % 2 == 0
+
+    def create_time(self) -> None:
+        # CREATE HOURS
+        hours = self.calculate_hours()
+        start_hour = hours["start"]
+        stop_hour = hours["stop"]
+
+        # CREATE MINUTES
+        minutes = self.calculate_minutes()
+        start_minutes = minutes["start"]
+        stop_minutes = minutes["stop"]
+
+        # CREATE SECONDS
+        seconds = self.calculate_seconds()
+        start_seconds = seconds["start"]
+        stop_seconds = seconds["stop"]
+
+        self.start_time = time(start_hour, start_minutes, start_seconds)
+        self.stop_time = time(stop_hour, stop_minutes, stop_seconds)
+
+    def calculate_hours(self) -> dict:
+        start_hour = random.randint(0, 11)
+        stop_hour = random.randint(start_hour, 12)
+
+        if self.is_second_entry_of_day():
+            # Second entry of day should be in the afternoon, cannot add 12 to prevent times like 24:10:00 -> invalid
+            start_hour += 11
+            stop_hour += 11
+
+        return {"start": start_hour, "stop": stop_hour}
+
+    @staticmethod
+    def calculate_minutes() -> dict:
+        # Needs to be this low to prevent times like 00:60:14 -> invalid
+        max_minutes = 58
+
+        start_minutes = random.randint(0, max_minutes)
+        stop_minutes = start_minutes + random.randint(1, 59 - start_minutes)
+
+        return {"start": start_minutes, "stop": stop_minutes}
+
+    @staticmethod
+    def calculate_seconds() -> dict:
+        # Needs to be this low to prevent times like 00:00:60 -> invalid
+        max_seconds = 58
+
+        start_seconds = random.randint(0, max_seconds)
+        stop_seconds = start_seconds + random.randint(1, 59 - start_seconds)
+
+        return {"start": start_seconds, "stop": stop_seconds}
+
+    def create_work_hours(self) -> None:
+        start_time = datetime.combine(datetime.min, self.start_time)
+        stop_time = datetime.combine(datetime.min, self.stop_time)
 
         time_delta = stop_time - start_time
         self.work_hours = time_delta
+
+    def create_message(self) -> None:
+        self.message = random.choice(self.messages)
