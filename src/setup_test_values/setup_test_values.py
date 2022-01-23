7ff795981f37bc9cba6155653dda5fd45d8b305a
Max <github-mg@guenther.cloud>
2022-01-23 16:20:52 +0100

    Update logic for setting up test values
    Needed to update the logic because of an error triggered when creating to many entries. This would lead into invlid dates like 'Jan, 34 2022' which caused actions like converting the date into a datetime.date() to fail.

--- src/setup_test_values/setup_test_values.py
+++ src/setup_test_values/setup_test_values.py
@@ -1,4 +1,8 @@
+import datetime
 import random
+
+import numpy
+import pandas
 
 from src.csv import CSVHandler
 from .entry import Entry
@@ -34,12 +38,33 @@ class SetupTestValues:
 
     def __entries(self) -> str:
         entries = ""
+        number_of_entries = self._number_of_entries
 
-        for i in range(1, self._number_of_entries + 1):
-            entries += self.__build_entry(i)
+        if number_of_entries % 2 != 0:
+            number_of_entries += 1
+
+        # Two entries per date, so two times fewer dates
+        number_of_entries /= 2
+
+        # Number of days in the past
+        today = datetime.datetime.now()
+        n_days_ago = today - datetime.timedelta(days=number_of_entries)
+
+        # Generate dates
+        dates = pandas.date_range(n_days_ago, today - datetime.timedelta(days=1), freq="d").to_pydatetime()
+
+        # Duplicate the dates
+        dates = numpy.repeat(dates, 2)
+
+        for i in range(0, len(dates)):
+            entries += self.__build_entry(dates[i], i)
+
+        if self._number_of_entries % 2 != 0:
+            entries = "\n".join(entries.split("\n")[:-2])
+            entries += "\n"
 
         return entries
 
-    def __build_entry(self, number: int) -> str:
+    def __build_entry(self, date: datetime.datetime, number: int) -> str:
         message = random.choice(self.__messages)
-        return self.__entry.build(message, number)
+        return self.__entry.build(date, message, number)
