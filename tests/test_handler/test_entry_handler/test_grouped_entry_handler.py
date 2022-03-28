6bf3c21754bca0993471eb8d93b1ff8c9bfeaa9b
Max <github-mg@guenther.cloud>
2022-03-28 19:49:47 +0200

    Refactor "test_grouped_entry_handler.py"

--- tests/test_handler/test_entry_handler/test_grouped_entry_handler.py
+++ tests/test_handler/test_entry_handler/test_grouped_entry_handler.py
@@ -37,27 +37,27 @@ class TestGroupedEntryHandler(CSVBaseTestingClass):
     def test_logging_with_more_grouped_entries(self) -> None:
         self.clean_and_init_tracker_file()
 
-        self.add_new_entry()
+        self.add_new_entries()
         self.assertEqual(self.get_length_of_grouped_entries(), 1)
 
         self.timer_handler.start_timer()
         self.timer_handler.stop_timer("")
 
         # Entries are grouped by date, so adding two or more entries at the same day won't increase the length
-        self.add_new_entry()
+        self.add_new_entries()
         self.assertEqual(self.get_length_of_grouped_entries(), 1)
 
-        self.add_new_entry()
-        self.add_new_entry()
+        self.add_new_entries(2)
         self.assertEqual(self.get_length_of_grouped_entries(), 1)
 
-    def add_new_entry(self):
-        self.timer_handler.start_timer()
-        self.timer_handler.stop_timer("")
+    def add_new_entries(self, number_of_entries: int = 1):
+        for i in range(1, number_of_entries + 1):
+            self.timer_handler.start_timer()
+            self.timer_handler.stop_timer("")
 
     def test_column_names_of_grouped_entries(self):
         self.clean_and_init_tracker_file()
-        self.add_new_entry()
+        self.add_new_entries()
 
         column_names = list(self.grouped_entry_handler.get_entries_grouped_by_date().columns)
         self.assertEqual(column_names, self.column_names)
