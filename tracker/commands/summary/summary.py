f1847f1e526d4b8ab566ebf33301073f0b2c8b84
Max <github-mg@guenther.cloud>
2022-04-02 11:34:09 +0200

    "tracker" should not be lowercase if it's at the beginning of a sentence

--- tracker/commands/summary/summary.py
+++ tracker/commands/summary/summary.py
@@ -1,9 +1,9 @@
 """This file is part of tracker.
-tracker is free software: you can redistribute it and/or modify
+Tracker is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-tracker is distributed in the hope that it will be useful,
+Tracker is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
