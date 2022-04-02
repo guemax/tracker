e0c3bfef096b7e8345c058c52b4b8b7ec844c612
Max <github-mg@guenther.cloud>
2022-04-02 09:46:20 +0200

    Add logging to main file and remove unused logging import statement

--- tracker/__main__.py
+++ tracker/__main__.py
@@ -10,13 +10,16 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Tracker. If not, see <http://www.gnu.org/licenses/>.
 """
+
 import logging
 
-from .setup_tracker_files import setup_tracker_files
-from .setup_logging import setup_logging
-from .setup_cli import setup_cli
-
 from .cli import cli
+
+from .setup_cli import setup_cli
+from .setup_logging import setup_logging
+from .setup_tracker_files import setup_tracker_files
+
+from .version import __version__
 
 
 def start_tracker():
@@ -27,9 +30,15 @@ def start_tracker():
 def main():
     setup_tracker_files()   # Needs to be done first to ensure a 'files'-folder exists for the log file!
     setup_logging()
+
+    logging.info("Initialising tracker")
     setup_cli(cli)
 
+    logging.info(f"Starting tracker {__version__}")
     start_tracker()
+
+    # TODO: Show exit information
+    # logging.info(f"Closing tracker {__version__}")
 
 
 if __name__ == "__main__":  # pragma: no cover
