50c73dbe7460be024a0cccc606889117bc199f56
Max <github-mg@guenther.cloud>
2022-06-05 20:11:08 +0200

    Make "Tracker" capitalised

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""This file is part of tracker.
+"""This file is part of Tracker.
 Tracker is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
--- tracker/setup_test_values/setup_test_values.py
+++ tracker/setup_test_values/setup_test_values.py
@@ -1,4 +1,4 @@
-"""This file is part of tracker.
+"""This file is part of Tracker.
 Tracker is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
